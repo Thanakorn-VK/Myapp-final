fdf11e6e1d4b35743b5e68f0e5c9717831e64453
Thanakorn-VK <thanakorn111@hotmail.com>
2020-08-07 14:46:35 +0700

    firebase succes

--- app/src/main/java/com/example/myapp/Ingredient.java
+++ app/src/main/java/com/example/myapp/Ingredient.java
@@ -1,13 +1,73 @@
 package com.example.myapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class Ingredient extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ingredient);
+        basicWrite();
+        basicRead();
+    }
+
+    public void basicWrite() {
+
+        // [START write_message]
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("id");
+
+        myRef.setValue("Hello, World!");
+
+        DatabaseReference myRef1 = database.getReference("message");
+
+        myRef1.setValue("Hello, Worldtest!");
+        // [END write_message]
+
+        // [START read_message]
+        // Read from the database
+        // [END read_message]
+        // Read from the database
+    }
+
+
+    public void basicRead(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("id");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                TextView textView = (TextView)findViewById(R.id.textdatatest);
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                textView.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+
+            }
+        });
     }
 }
