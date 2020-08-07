189832382dffb94053ebe45cd4d2adfcdb23c783
Thanakorn-VK <thanakorn111@hotmail.com>
2020-08-07 17:18:10 +0700

    read/write date and show data

--- app/src/main/java/com/example/myapp/Ingredient.java
+++ app/src/main/java/com/example/myapp/Ingredient.java
@@ -1,10 +1,9 @@
 package com.example.myapp;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,17 +14,69 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
+
 
 public class Ingredient extends AppCompatActivity {
+    String v="test";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ingredient);
-        basicWrite();
-        basicRead();
+        Toast.makeText(getApplicationContext(),"read data",Toast.LENGTH_LONG).show();
+        readdata();
+
+
+
     }
 
+
+    public void setname() {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("ingredient/01/name");
+        Toast.makeText(getApplicationContext(),"readed path",Toast.LENGTH_LONG).show();
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //TextView textView = (TextView)findViewById(R.id.box1);
+                v = dataSnapshot.getValue(String.class);
+                //textView.setText(v);
+                Toast.makeText(getApplicationContext(),"onDataChage",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+
+            }
+        });
+    }
+    public void readdata() {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("ingredient/01/check");
+        Toast.makeText(getApplicationContext(),"get path",Toast.LENGTH_LONG).show();
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                TextView textView = (TextView)findViewById(R.id.box1);
+                String value = dataSnapshot.getValue(String.class);
+                Toast.makeText(getApplicationContext(),"get string",Toast.LENGTH_LONG).show();
+                if(value.equals("yes")) {
+                   Toast.makeText(getApplicationContext(),"compair",Toast.LENGTH_LONG).show();
+                    setname();
+                    Toast.makeText(getApplicationContext(),"set box1",Toast.LENGTH_LONG).show();
+                    textView.setText(v);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
+
+    }/*
     public void basicWrite() {
 
         // [START write_message]
@@ -46,7 +97,7 @@ public class Ingredient extends AppCompatActivity {
         // Read from the database
     }
 
-
+    /*
     public void basicRead(){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("id");
@@ -69,5 +120,5 @@ public class Ingredient extends AppCompatActivity {
 
             }
         });
-    }
+    }*/
 }
