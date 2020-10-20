4612fb832ca350e7bd0d77a50c3328fbc7b73daf
Thanakorn-VK <tanagorn111@hotmail.com>
2020-10-20 21:12:49 +0700

    Home page

--- app/src/main/java/com/example/myapp/Home.java
+++ app/src/main/java/com/example/myapp/Home.java
@@ -18,5 +18,37 @@ public class Home extends AppCompatActivity {
         }catch (NullPointerException e){
 
         }
+        Button logo1 = (Button) findViewById(R.id.logo1);
+        logo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Ingredient.class);
+                startActivity(i);
+            }
+        });
+        Button logo2 = (Button) findViewById(R.id.logo2);
+        logo2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Chooseingredient.class);
+                startActivity(i);
+            }
+        });
+        Button logo3 = (Button) findViewById(R.id.logo3);
+        logo3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Favourite.class);
+                startActivity(i);
+            }
+        });
+        Button logo4 = (Button) findViewById(R.id.logo4);
+        logo4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
